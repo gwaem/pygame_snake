f846656e1c867cffb4952dc5324d2b6968acc317
Gunnar Waem <160498180+gwaem@users.noreply.github.com>
2026-09-21 17:53:58 +0200

    refactor: rewrite tilemap.py

--- scripts/tilemap.py
+++ scripts/tilemap.py
@@ -1,28 +1,18 @@
-import pygame
-
-
 class Tilemap:
     def __init__(self, game, tile_size=16):
         self.game = game
         self.tile_size = tile_size
-        self.tiles = []
-
-        for y in range(self.game.TILES_Y):
-            row = []
-            for x in range(self.game.TILES_X):
-                row.append(0)
-            self.tiles.append(row)
 
     def render(self, surface):
-        for y, row in enumerate(self.tiles):
-            for x, tile in enumerate(row):
+        # Render the tilemap on screen.
+        for y in range(self.game.TILES_Y):
+            for x in range(self.game.TILES_X):
                 if (x + y) % 2 == 0:
-                    surface.blit(
-                        self.game.assets["tiles"]["tile_0.png"],
-                        (x * self.tile_size, y * self.tile_size),
-                    )
+                    tile = self.game.assets["tiles"]["tile_0.png"]
                 else:
-                    surface.blit(
-                        self.game.assets["tiles"]["tile_1.png"],
-                        (x * self.tile_size, y * self.tile_size),
-                    )
+                    tile = self.game.assets["tiles"]["tile_1.png"]
+
+                surface.blit(
+                    tile,
+                    (x * self.tile_size, y * self.tile_size),
+                )
